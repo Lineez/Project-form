d9963858b5a3906d1639b96944032d341107a77e
Egor <qewqew26@mail.ru>
2021-02-04 13:05:38 +0300

    v1

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const fileinclude = require('gulp-file-include');
-const svgSprite = require('gulp-svg-sprite');
 // const ttf2woff2Gulp = require('gulp-ttf2woff2');
 
 let project_folder = "docs";
@@ -22,7 +21,7 @@ let path = {
 		pages: [source_folder + '/pages/**/*.html'],
 		css: source_folder + '/scss/**/*.scss',
 		js: source_folder + '/js/script.js',
-		img: [source_folder + '/img/**/*.{jpg,png,svg,gif,ico,webp}', "!" + source_folder + '/img/stack/*.svg'],
+		img: [source_folder + '/img/**/*.{jpg,png,svg,gif,ico,webp}'],
 		fonts: source_folder + '/fonts/*.woff2',
 		iconFonts: source_folder + '/fonts/icon-font/**/*.*',
 	},
@@ -53,7 +52,6 @@ let {
 	rename = require('gulp-rename'),
 	uglify = require('gulp-uglify-es').default,
 	imagemin = require('gulp-imagemin'),
-	svg = require('gulp-svg-sprite'),
 	ttf2woff = require('gulp-ttf2woff'),
 	ttf2woff2 = require('gulp-ttf2woff2'),
 	fonter = require('gulp-fonter'); // otf fonts converter
@@ -86,6 +84,7 @@ function fonts() {
 // 	.pipe(ttf2woff())
 // 	.pipe(dest(path.build.fonts));
 // }
+
 // иконочный шрифт
 function iconFonts() {
 	return src([source_folder + '/fonts/icon-font/**/*.*'])
@@ -126,21 +125,6 @@ function images() {
 		.pipe(dest(path.build.img))
 		.pipe(browsersync.stream())
 }
-// Собирает svg файлы в один спрайт
-gulp.task('svg', function () {
-	return gulp.src([source_folder + '/img/stack/*.svg'])
-		.pipe(svg({
-			mode: {
-				stack: {
-						sprite: 'sprite.svg',
-					// sprite: '../icons/icons.svg',
-					// 
-					// example: true,
-				}
-			},
-		}))
-		.pipe(dest(path.build.img))
-})
 
 function html() {
 	return src(path.src.html)
@@ -201,8 +185,8 @@ function clean() {
 	return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(fonts, iconFonts, html, pages, css, js, images));
-let watch = gulp.parallel(build, watchFIles, browserSync);
+let build = gulp.series(clean, gulp.parallel(fonts, iconFonts, html, pages, css, js));
+let watch = gulp.parallel(build, watchFIles, browserSync, images);
 
 
 exports.images = images;
